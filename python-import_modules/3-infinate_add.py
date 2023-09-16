d8c301c131aece1f35321e259491b2b494c12829
TPoe25 <t_poe25@yahoo.com>
2023-09-15 22:56:47 -0700

    fix infinate add

--- python-import_modules/3-infinate_add.py
+++ python-import_modules/3-infinate_add.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+import sys
 
 if __name__ == "__main__":
 
-    import sys
-    
     total = 0
     
-for i in range(len(sys.argv) - 1):
-    total += int(sys.argv[i + 1])
+for arg in sys.argv[1:]:
+    try:
+        total += int(arg)
+    except ValueError:
+        pass
 
 print("{}".format(total))
