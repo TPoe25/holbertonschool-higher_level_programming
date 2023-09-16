97319721e3392618be9a702ce586b83c8b0fec9d
TPoe25 <t_poe25@yahoo.com>
2023-09-15 21:51:44 -0700

    fix

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-#for looping through the alphabet and ord ascii characters
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print("{}".format(chr(i)), end='')
-        
-print()
+output = ""
+
+for char in "abcdefghijklmnopqrstuvwxyz":
+    if char != 'e' and char != 'q':
+        output += char
+
+print(output)
