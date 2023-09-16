6c26c7e987e198464b134c63c064d3562dc14c64
TPoe25 <t_poe25@yahoo.com>
2023-09-15 21:04:08 -0700

    import

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+with open("hidden_4.pyc", "rb") as f:
+    code = f.read()
+    
+lines = code.split(b"\n")
+
+names = set()
+for line in lines:
+    if b"STORE_NAME" in line and not line.startswith(b"__"):
+        name = line.spilt(b" ")[-1].strip()
+        names.add(name.decode("utf-8"))
+        
+for name in sorted(names):
+    print(name) 
