0f7074646e36c147f449a3e150fd2b4356c63aa4
TPoe25 <t_poe25@yahoo.com>
2023-09-19 18:14:42 -0700

    4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -2,9 +2,11 @@
 """" this is square class """
 
 class Square:
+    """ square class """
     def __init__(self, size=0):
         """ creates a square object """
-        self.__size = size
+        """ Args: size: the size of the square """
+        self.size = size
   
     @property
     def size(self):
@@ -18,12 +20,11 @@ class Square:
         """ Args: value: the size of the square """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
     
     def area(self):
-            """ calculates the area of the square """
-            """ Return: the area of the square """
-            return self.__size ** 2
+        """ calculates the area of the square """
+        """ Return: the area of the square """
+        return self.__size ** 2
