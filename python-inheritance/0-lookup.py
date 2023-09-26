0ff2be8cf2f2c1000cc43601bc145e901e4cccbf
TPoe25 <t_poe25@yahoo.com>
2023-09-26 14:16:00 -0700

    checking

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" define a object lookup function"""
 
 def lookup(obj):
+    """ return a list of all attributes of an object"""
     return dir(obj)
--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ This function prints the elements of a list in sorted order."""
+
 class MyList(list):
     """ MyList is a subclass of list. """
     def print_sorted(self):
-        """ print_sorted() prints the elements of list in sorted order."""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        if issubclass(MyList, list):
+            print(sorted(self))
