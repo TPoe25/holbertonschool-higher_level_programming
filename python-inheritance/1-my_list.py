e1e8f5f435bdfd62b7021cce2482246b34ffefe9
TPoe25 <t_poe25@yahoo.com>
2023-09-26 14:57:24 -0700

    checking

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,5 +5,8 @@ class MyList(list):
     """ MyList is a subclass of list. """
     def print_sorted(self):
         """ Prints the elements of the list in sorted order."""
-        if issubclass(MyList, list):
-            print(sorted(self))
+        print(sorted(self))
+        
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("my_list.txt")
