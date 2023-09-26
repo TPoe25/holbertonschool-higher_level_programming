cc1cb114ed6a51cb030d255d301656dd694d23b0
TPoe25 <t_poe25@yahoo.com>
2023-09-26 15:26:00 -0700

    check

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-""" This function prints the elements of a list in sorted order."""
+"""
+This function prints the elements of a list in sorted order
+"""
 
 class MyList(list):
-    """ MyList is a subclass of list. """
+    """
+    MyList is a subclass of list with custom print_sorted method
+    """
     def print_sorted(self):
-        """ Prints the elements of the list in sorted order."""
+        """
+        Prints the elements of the list in sorted order
+        """
         print(sorted(self))
         
 if __name__ == "__main__":
-    import doctest
-    doctest.testfile("my_list.txt")
+    pass
