b80de56e19e73a3d679ab8f4cca6bebc64fce823
TPoe25 <t_poe25@yahoo.com>
2023-09-27 09:29:09 -0700

    commit

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,52 @@
 #!/usr/bin/python3
 
+
 def add_integer(a, b = 98):
+    """Adds two integers together and returns the sum.
+
+    Args: 
+        a (int or float): first int or float
+        b (int or float, optional): second int or float. Default to 98.
+
+    Returns:
+        int: sum of a and b as integer.
+
+    Example:
+        >>> add_integer(1, 2)
+        3
+        >>> add_integer(100, -2)
+        98
     """
+
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    return int(a) + int(b)
+    Adds two integers together and returns the sum.
+
+    Args: 
+        a (int or float): first int or float
+        b (int or float, optional): second int or float. Default to 98.
+    Adds two integers together and returns the sum.
+
+    Args: 
+        a (int or float): first int or float
+        b (int or float, optional): second int or float. Default to 98.
+
+    Returns:
+        int: sum of a and b as integer.
+
+    Example:
+        >>> add_integer(1, 2)
+        3
+        >>> add_integer(100, -2)
+        98
+    """
+
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    return int(a) + int(b)
     Adds two integers together and returns the sum.
 
     Args: 
