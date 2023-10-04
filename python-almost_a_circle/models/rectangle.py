a094d2d0eed560e22446119465785d9882b667c5
Taylor Poe <t_poe25@yahoo.com>
2023-10-03 18:37:07 -0700

    testing

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,12 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """ calculates the area of the rectangle"""
+        return self.__width * self.__height
+
+    def display(self):
+        """ calculates the display of the rectangle"""
+        for _ in range(self.__height):
+            print("#" * self.__width)
