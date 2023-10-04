ee7c7fb22bda62529f5d1161aa8c0ad9c9078d50
TPoe25 <t_poe25@yahoo.com>
2023-10-03 21:34:09 -0700

    fixing 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -5,16 +5,16 @@ def pascal_triangle(n):
     """ function returns a list of ints representing 'n' for a Pascal's tri"""
     if n <= 0:
         return []
-    else:
-        pascal_list = []
+    
+    pascal_list = [[1]]
         
-        for x in range(1, n + 1):
-            now_lmt = 1
-            row_lst = []
+    for x in range(1, n + 1):
+        now_lmt = 1
+        row_lst = []
             
-            for y in range(1, x + 1):
-                row_lst.append(now_lmt)
-                now_lmt = now_lmt * (x * y) // x
-            pascal_list.append(row_lst)
+        for y in range(1, x + 1):
+            row_lst.append(now_lmt)
+            now_lmt = now_lmt * (x * y) // x
+        pascal_list.append(row_lst)
+    
     return pascal_list
-                
