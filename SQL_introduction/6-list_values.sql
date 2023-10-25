1cfa593d5c561eaedc240a281e5361d6e115abdc
TPoe25 <t_poe25@yahoo.com>
2023-10-24 18:11:21 -0700

    fixing 7

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,6 +1,2 @@
--- List all rows of the first_table
-USE tables;
-
 -- Get all rows from the first_table
-SELECT *
-FROM first_table;
+SELECT * FROM first_table;
--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,6 +1,2 @@
--- Insert a new row to the first table
-USE tables;
-
 -- Put into the new row
-INSERT INTO first_table (id, name)
-VALUES (89, 'Best School');
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
