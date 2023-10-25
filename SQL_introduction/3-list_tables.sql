252a0d8ad9bdec4814a033b0b607be32f64dce2d
TPoe25 <t_poe25@yahoo.com>
2023-10-24 17:58:55 -0700

    fixing

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,9 +1,5 @@
 -- List all tables in database
-
--- connects to mysql server
 USE mysql;
 
--- Retrieve table names for information_schema
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = DATABASE();
+-- list all tables
+SHOW TABLES;
--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,3 @@
--- Use database
-USE hbtn_0c_0;
-
 -- Creates the first_table if it doesnt exist
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
