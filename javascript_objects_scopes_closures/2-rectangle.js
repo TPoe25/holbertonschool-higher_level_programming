ac631c87cfcb73a92d4f5007a5a326f898eceba9
TPoe25 <t_poe25@yahoo.com>
2023-12-09 17:31:17 -0800

    2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,14 +2,13 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      // creates empty object if w or h is >= 0
-      return {};
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      // creates empty object if w or h is >= 0 or not a positive integer
+    } else {
+      // Initalizing instance of width and height w/ their values
+      this.width = w;
+      this.height = h;
     }
-
-    // Initalizing instance of width and height w/ their values
-    this.width = w;
-    this.height = h;
   }
 }
 
