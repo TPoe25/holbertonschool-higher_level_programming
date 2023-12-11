7f93d011b8e145510f5efeb84a1eecffa2937ac1
TPoe25 <t_poe25@yahoo.com>
2023-12-11 00:33:25 -0600

    had trouble all day with project had breakthru

---
+++ javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    const updateHeaderElement = $("#update_header");
+    updateHeaderElement.click(function() {
+        const headerElement = $("header");
+        headerElement.text("New Header!!!");
+    });
+});
